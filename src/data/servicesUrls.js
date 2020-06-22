b9db06f564d1db768eb07d4bd1b2621438976d7b
Lasya Sri Paravastu <lasyas@192-168-1-102.tpgi.com.au>
2020-06-23 02:52:33 +1000

    added push production configuration

--- src/data/servicesUrls.js
+++ src/data/servicesUrls.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "https://gds-chat-server.herokuapp.com";
+//export const BASE_URL = "https://gds-chat-server.herokuapp.com";
 //http://192.168.1.2:8080";
-//export const BASE_URL = 'https://gds-chat-server-prod.herokuapp.com';//''https://gds-chat-server.herokuapp.com';
+export const BASE_URL = 'https://gds-chat-server-prod.herokuapp.com';//''https://gds-chat-server.herokuapp.com';
 export const CONVERSATIONS_URL = BASE_URL + '/users/conversations';
 export const MESSAGES_URL = BASE_URL + '/conversations/messages';
 export const SEND_MESSAGE_URL = BASE_URL + '/messages';
